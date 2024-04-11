52f5f35e66a08bf38584cec8686c0cc1740ed745
00011244wiut <umidjonaskarov@yandex.com>
2024-04-11 22:02:48 +0300

    fetch sales

--- pages/merchant/sales.vue
+++ pages/merchant/sales.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { format, parseISO } from "date-fns";
+
 const sales = ref([]);
 const { requestData, loading } = useApi();
 const getSales = async () => {
 	try {
 		await requestData("get", "sales/bymerchant").then((res) => {
-			sales.value = res;
+			sales.value = res.sales;
 		});
 	} catch (error) {
 		console.log(error);
@@ -22,16 +24,19 @@ definePageMeta({
 <template>
 	<div class="h-full px-28 space-y-20">
 		<h1 class="text-4xl">Sales</h1>
-		<div class="flex items-center gap-x-6">
+		<di v-for="sale in sales" :key="sale" v class="flex items-center gap-x-6">
 			<div
 				class="bg-[#92D7A5] w-[57px] h-[57px] rounded-full flex justify-center items-center"
 			>
 				<Icon name="fa-solid:star-of-life" class="text-2xl" />
 			</div>
 			<div class="space-y-2">
-				<p class="text-2xl">Product Name</p>
-				<p>600 000 UZS . <span>DateTime</span></p>
+				<p class="text-2xl">{{ sale.productName }}</p>
+				<p>
+					{{ sale.saleAmount }} UZS . <span>DateTime: </span
+					>{{ format(parseISO(sale.createdTime), "MMMM dd") }}
+				</p>
 			</div>
-		</div>
+		</di>
 	</div>
 </template>
